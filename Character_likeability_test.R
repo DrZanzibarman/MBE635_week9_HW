5f680889355c47670bece32bf429576209a659e6
Ben Gulmon Mobile <ben.gulmon@gmail.com>
2021-10-26 14:29:37 -0400

    basic function had been created.  Need to update it with filter and maybe facetwrap

--- Character_likeability_test.R
+++ Character_likeability_test.R
@@ -1,3 +1,17 @@
+mybarplot <- function(dataframe, x_axis, y_axis, fill_factor) {
+  ggplot2::ggplot(data = {{dataframe}})+
+    geom_bar(mapping = aes(x= {{x_axis}},
+                           fill = {{fill_factor}}
+                          ),
+                    stat = {{y_axis}},
+                    position = 'dodge'
+  )
+}
+
+mybarplot(SWars_Clean, View_Han, 'count', Gender)
+
+
+
 ggplot(data = SWars_Clean)+
   geom_bar(mapping = aes(x= View_Luke,
                          fill = Gender
